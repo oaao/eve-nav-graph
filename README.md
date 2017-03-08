eea53df927fe13d963c7a454d87cf2aa5c4d2124
oaao <oliver.antoniu@gmail.com>
2017-03-08 14:34:26 -0500

    updated scope and intent

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 A navigation resource which provides pre-computed shortest paths for travel in EVE Online.
 Its purpose is to circumvent ceiling issues such as a limited number of waypoints. 
 
-Data will be formulated via python using networkx and stored in a neo4j graph database.
+The graph is constructed by processing EVE SDE data in the networkx library for Python.
+
+Shortest paths will be calculated using Dijkstra's algorithm.
+Final data will be stored in a neo4j graph database.
--- build_graph.py
+++ build_graph.py
@@ -2,10 +2,6 @@ import json
 import networkx as nx
 
 
-def load_json(data):
-    return json.load(open(data))
-
-
 # This function is unnecessary to use in this specific case given that the available data provides "edges".
 def nodes_from_data(data, k):
     nodes = [x[k] for x in data]
@@ -16,9 +12,10 @@ def edges_from_data(data, k1, k2):
     edges = [(x[k1], x[k2]) for x in data]
     return edges
 
-jumps_data = load_json('jumps.json')
-jumps      = edges_from_data(jumps_data, 'fromSolarSystemID', 'toSolarSystemID')
+if __name__ == "__main__":
+    jump_data = json.load(open('jumps.json'))
+    jumps     = edges_from_data(jump_data, 'fromSolarSystemID', 'toSolarSystemID')
 
-G = nx.Graph()
-G.add_edges_from(jumps)
-print(nx.info(G))
+    G = nx.Graph()
+    G.add_edges_from(jumps)
+    print(nx.info(G))
