f2bc602cf7daa4db77e3a72aaf81e01bdf17a88f
oaao <oliver.antoniu@gmail.com>
2017-05-23 12:18:43 -0400

    rev: naming - from_nodes, from_edges

--- build_graph.py
+++ build_graph.py
@@ -3,19 +3,21 @@ import networkx as nx
 
 
 # This function is unnecessary to use in this specific case given that the available data provides "edges".
-def nodes_from_data(data, k):
+def from_nodes(data, k):
     nodes = [x[k] for x in data]
     return nodes
 
 
-def edges_from_data(data, k1, k2):
+def from_edges(data, k1, k2):
     edges = [(x[k1], x[k2]) for x in data]
     return edges
 
 if __name__ == "__main__":
-    jump_data = json.load(open('jumps.json'))
-    jumps     = edges_from_data(jump_data, 'fromSolarSystemID', 'toSolarSystemID')
+    jump_data = json.load(open('res/jumps.json'))
+    jumps     = from_edges(jump_data, 'fromSolarSystemID', 'toSolarSystemID')
 
     G = nx.Graph()
     G.add_edges_from(jumps)
     print(nx.info(G))
+    print(nx.density(G))
+    print(type(G))
